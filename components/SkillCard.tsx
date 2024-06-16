12e29d18594022f9001c9cf5de3dc3ccec646212
Ibrahim Ahmed <ahmed1768476@gmail.com>
2024-06-16 21:33:35 +0100

    bug fixes

--- components/SkillCard.tsx
+++ components/SkillCard.tsx
@@ -3,11 +3,11 @@ import { SkillCardProps } from "@types";
 function SkillCard({ title, image }: SkillCardProps) {
   return (
     <article
-      className="object-contain flex items-center justify-between hover:cursor-pointer hover:scale-110 transition duration-200 mt-8 px-4 lg:px-8 py-4 h-20 sm:h-24 lg:h-28 xl:h-32 w-[48%] sm:w-[32%] lg:w-[23%] bg-gradient-to-l from-[#c6c9c8]  dark:from-[#1f1f1f] to-navbar-light hover:to-[#c6c9c8] rounded-2xl dark:to-navbar-dark hover:dark:to-[#1f1f1f] shadow-md hover:shadow-xl"
+      className="flex items-center justify-between hover:cursor-pointer hover:scale-110 transition duration-200 mt-8 px-4 lg:px-8 py-4 h-20 sm:h-24 lg:h-28 xl:h-32 w-[48%] sm:w-[32%] lg:w-[23%] bg-gradient-to-l from-[#c6c9c8]  dark:from-[#1f1f1f] to-navbar-light hover:to-[#c6c9c8] rounded-2xl dark:to-navbar-dark hover:dark:to-[#1f1f1f] shadow-md hover:shadow-xl"
       data-aos="fade-up"
     >
       <div className="h-full">
-        <img src={image} alt={title} className="w-full h-full" />
+        <img src={image} alt={title} className="w-full h-full object-contain" />
       </div>
       <p className="font-bold text-sm sm:text-base xl:text-xl text-site-text-gray shrink">
         {title}
